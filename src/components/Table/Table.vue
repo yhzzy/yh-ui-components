f995a73424d6c1c9bae3b2aebb7d059b4f984af7
176145910@qq.com <176145910@qq.com>
2019-10-10 16:41:04 +0800

    添加table组件

---
+++ src/components/Table/Table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="yh-ui yh-table">
+    <el-table :data="tableData" v-bind="{ ...elementConfig }" style="width: 100%">
+      <el-table-column
+        v-for="column in columns"
+        :key="column.prop"
+        :prop="column.prop"
+        label="日期"
+        v-bind="{ ...column.elementConfig }"
+      ></el-table-column>
+      <slot name="temp"></slot>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: {
+    tableData: {
+      type: Array,
+      required: true,
+    },
+    elementConfig: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    columns: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
